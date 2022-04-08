89fcfc931c0658cad2bf6420e29664c4b4a11b74
guachilimbo <84127524+guachilimbo@users.noreply.github.com>
2022-04-08 11:40:22 +0100

    Edited a failed test.

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -7,7 +7,7 @@ public class BestLoanRates {
     public static final Map<Integer, Float> bestRates = Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: ");
+        System.out.println("Enter your name");
         String name = scanner.nextLine();
         System.out.println("Hello "+name);
         System.out.println("Enter the loan term (in years)");
